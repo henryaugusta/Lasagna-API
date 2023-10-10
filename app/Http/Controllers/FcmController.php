0753909c756d018ffe122882c3bdfa04a8b998bc
Henry Augusta <henryaugusta4@gmail.com>
2023-10-10 15:37:15 +0700

    fix blast message error.

--- app/Http/Controllers/FcmController.php
+++ app/Http/Controllers/FcmController.php
@@ -2,12 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use GuzzleHttp\Client;
 use App\Models\DeviceFCM;
 use Illuminate\Http\Request;
 
 class FcmController extends Controller
 {
-    public function storeNewDevice(Request $request){
+    public function storeNewDevice(Request $request)
+    {
         $device = $request->device;
         $username = $request->username;
         $token = $request->token;
@@ -29,11 +31,58 @@ class FcmController extends Controller
             $data->token = $token;
             $data->save();
         }
-
         return response()->json(['message' => 'Device data stored successfully']);
     }
 
-    public function getFcm(Request $request){
-        return DeviceFCM::all();
+    public function getFcm(Request $request)
+    {
+        $datas = DeviceFCM::distinct('token')->get();
+
+        $results = [];
+
+        foreach ($datas as $data) {
+            $curl = curl_init();
+            $token = $data->token;
+
+            curl_setopt_array($curl, array(
+                CURLOPT_URL => 'https://fcm.googleapis.com/fcm/send',
+                CURLOPT_RETURNTRANSFER => true,
+                CURLOPT_ENCODING => '',
+                CURLOPT_MAXREDIRS => 10,
+                CURLOPT_TIMEOUT => 0,
+                CURLOPT_FOLLOWLOCATION => true,
+                CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+                CURLOPT_CUSTOMREQUEST => 'POST',
+                CURLOPT_POSTFIELDS =>'{
+                "notification" :{
+                    "title": "EMERGENCY",
+                    "body": "Ada Orang Jatuh di Kamar Mandi, Segera Berikan Pertolongan",
+                    "click_action" :"FLUTTER_NOTIFICATION_CLICK",
+                    "sound": "manydocwaiting.caf",
+                    "badge" :4,
+                    "content_available" : true,
+                    "mutable_content":true
+                },
+                "data":{
+                    "name":"Wahyudi Atkinson Rohadi",
+                    "age":25
+                },
+                "to":"' . $token . '"
+            }',
+                CURLOPT_HTTPHEADER => array(
+                    'Content-Type: application/json',
+                    'Authorization: key=AAAA9dJJOgM:APA91bEuhXVL6lgh7yPfknMYMSriWSv2LcDdSoMQakeZe3NiqfweYMHAPgyDifC7cHWUFvJL3ZvHeDCZ92wiSEHJyRr1fj3Su4o4X4k5bhubXfogqBQcguGF1ZHz3fqpaFM2Lsp729Q2'
+                ),
+            ));
+
+            $response = curl_exec($curl);
+
+            // Assuming you want to collect the response for each request
+            $results[] = json_decode($response, true);
+
+            curl_close($curl);
+        }
+
+        return response()->json(['results' => $results]);
     }
 }
